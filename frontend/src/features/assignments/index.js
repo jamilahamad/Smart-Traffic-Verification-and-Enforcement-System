fcf263e4f5f16d02d3bea888899ca411699bd206
Md. Jamil Ahamad AlAmin <jamilahamad06alamin@gmail.com>
2026-07-01 08:56:25 +0600

    chore(features): add assignments feature export

---
+++ frontend/src/features/assignments/index.js
@@ -0,0 +1,14 @@
+export { default as assignmentApi } from '../../services/assignmentApi';
+
+export {
+  getVehicleId,
+  getUserId,
+  idsMatch,
+} from '../../utils/getId';
+
+export {
+  USER_STATUSES,
+  VEHICLE_STATUSES,
+  getStatusLabel,
+  getStatusVariant,
+} from '../../constants/statuses';
